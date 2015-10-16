b41cf34f61c1d60d217a0023360e51a7c8f9c128
Charles Harrington <charles.harrington@gmail.com>
2015-10-16 15:57:56 -0700

    Draw enemies in svg element. Enemies move every 1 second

--- client/watchout.js
+++ client/watchout.js
@@ -1,1 +1,80 @@
 // start slingin' some d3 here.
+
+// Setup the game
+var h = 500;
+var w = 500;
+var enemies = [];
+
+var randomLocation = function(axis) { // axis is h or w
+  return Math.floor(Math.random() * axis - 5);
+};
+
+// Initialize enemies
+for (var i = 0; i < 10; i++) {
+  var enemy = {
+    id: i,
+    xloc: randomLocation(w),
+    yloc: randomLocation(h)
+  };
+  enemies.push(enemy);
+}
+
+
+// Random location
+setInterval(function(){
+
+  for (var enemy = 0; enemy < enemies.length; enemy++ ){
+    console.log(enemies[0]);
+    enemies[enemy].xloc = randomLocation(w);
+    enemies[enemy].yloc = randomLocation(h);
+  }
+  updateEnemies();
+},1000);
+
+
+// Prevent from offscreen
+
+// CREATE GAME BOARD
+var svg = d3.select("body")
+  .append("svg")
+  .attr("height", h)
+  .attr("width", w);
+
+
+// CREATE ENEMIES
+var createEnemies = function() {
+
+  svg.selectAll("circle")
+    .data(enemies)
+    .enter()
+    .append("circle")
+    .attr("cx", function(d){
+      return d.xloc;
+    })
+    .attr("cy", function(d){
+      return d.yloc;
+    })
+    .attr("r", 10)
+    .attr("class", "enemy");
+};
+
+var updateEnemies = function() {
+
+  svg.selectAll("circle")
+    .data(enemies, function(d){
+      return d.id;
+    })
+    .transition()
+    .duration(750)
+    .attr("cx", function(d){
+      return d.xloc;
+    })
+    .attr("cy", function(d){
+      return d.yloc;
+    })
+    .attr("r", 10);
+
+};
+
+createEnemies();
+console.log(enemies);
